e85460fdfbc4a00d98ea734407a40c8623efe411
Dianne Boholst <yeogiyeogi394@gmail.com>
2026-05-06 20:31:36 +0800

    update app portfolio

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { App } from "./App";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import Home from './pages/Home';
+import { ProjectDetail } from './pages/ProjectDetail';
+import { BlogDetail } from './pages/BlogDetail';
+import { TalkDetail } from './pages/TalkDetail';
+import { CompetitionDetail } from './pages/CompetitionDetail';
+import { ProjectsList } from './pages/ProjectsList';
+import { BlogList } from './pages/BlogList';
+import { CompetitionsList } from './pages/CompetitionsList';
+
 export function AppRouter() {
-  return <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-          </Routes>
-      </BrowserRouter>;
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectsList />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="/blog" element={<BlogList />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+          <Route path="/talks/:id" element={<TalkDetail />} />
+          <Route path="/competitions" element={<CompetitionsList />} />
+          <Route path="/competitions/:id" element={<CompetitionDetail />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
